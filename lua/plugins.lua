fa102c05e28d9d53edeaceedce55bbc2eb1c369c
Nabil-Alhanif <nabil.alhanif@protonmail.com>
2023-01-26 11:02:04 +0700

    Migrate packer to lazy.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,53 +1,46 @@
-local execute = vim.api.nvim_command
-local fn = vim.fn
-
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-local packer_bootstrap = false
-
-if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable',
+        lazypath,
+    })
 end
+vim.opt.rtp:prepend(lazypath)
 
-vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
-
-return require('packer').startup(function(use)
-
-    -- Package Manager
-    use 'wbthomason/packer.nvim'       -- Package manager written in lua
-
-    -- TreeSitter
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate',
-    }
-
-    -- Explorer
-    use {
-        'nvim-neo-tree/neo-tree.nvim',
-        branch = 'v2.x',
-        requires = {
-            'nvim-lua/plenary.nvim',
-            'nvim-tree/nvim-web-devicons',
-            'MunifTanjim/nui.nvim',
+require('lazy').setup({
+	{
+		'nvim-treesitter/nvim-treesitter',
+		build = ':TSUpdate',
+	},
+	{
+		'nvim-neo-tree/neo-tree.nvim',
+		branch = 'v2.x',
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			'nvim-tree/nvim-web-devicons',
+			'MunifTanjim/nui.nvim',
+		},
+	},
+	{
+		'nvim-lua/plenary.nvim',
+	},
+	{
+		'christianchiarulli/nvcode-color-schemes.vim',
+	},
+	{
+		'folke/tokyonight.nvim',
+	},
+    {
+        'neovim/nvim-lspconfig',
+    },
+    {
+        'williamboman/mason.nvim',
+        dependencies = {
+            'williamboman/mason-lspconfig.nvim',
         },
-    }
-    use 'nvim-tree/nvim-web-devicons' -- Icons for nvim-tree
-
-    -- LSP
-    use 'neovim/nvim-lspconfig'
-    use 'williamboman/mason.nvim'
-    use 'williamboman/mason-lspconfig.nvim'
-
-    -- Lua functions
-    use 'nvim-lua/plenary.nvim'
-
-    -- Theme
-    use 'christianchiarulli/nvcode-color-schemes.vim'
-    use 'folke/tokyonight.nvim'
-    use 'lukas-reineke/indent-blankline.nvim'
-
-    if packer_bootstrap then
-        require('packer').sync()
-    end
-
-end)
+    },
+})
