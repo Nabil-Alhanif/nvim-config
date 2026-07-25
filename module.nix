b89e9fc05b7bb51ecc9f261d11b5f03ceb547361
Nabil-Alhanif <nabil.alhanif@protonmail.com>
2026-07-25 13:10:54 -0700

    feat(nix): add nix-wrapper-modules flake infrastructure
    Add flake.nix and module.nix implementing a portable Neovim wrapper
    via nix-wrapper-modules. All plugins, LSP servers, formatters, and
    tools are bundled into a single derivation.
    
    flake.nix: standard nix-wrapper-modules template with package,
    overlay, NixOS/home-manager module outputs. Inputs include nixpkgs
    (unstable), nix-wrapper-modules, telescope-zotero.nvim, and sqlite.lua.
    
    module.nix: imports neovim wrapper module and declares:
    - 23 plugins from nixpkgs vimPlugins
    - 23 tree-sitter grammars via nvim-treesitter.withPlugins
    - 2 external plugins (telescope-zotero, sqlite.lua) via flake inputs
    - 11 LSP servers as runtime deps
    - 8 formatters and tools as runtime deps

---
+++ module.nix
@@ -0,0 +1,141 @@
+inputs:
+{
+  config,
+  wlib,
+  lib,
+  pkgs,
+  options,
+  ...
+}:
+{
+  imports = [ wlib.wrapperModules.neovim ];
+
+  options.nvim-lib.neovimPlugins = lib.mkOption {
+    readOnly = true;
+    type = lib.types.attrsOf wlib.types.stringable;
+    default = config.nvim-lib.pluginsFromPrefix "plugins-" inputs;
+  };
+
+  options.nvim-lib.pluginsFromPrefix = lib.mkOption {
+    type = lib.types.raw;
+    readOnly = true;
+    default =
+      prefix: inputs:
+      lib.pipe inputs [
+        builtins.attrNames
+        (builtins.filter (s: lib.hasPrefix prefix s))
+        (map (
+          input:
+          let
+            name = lib.removePrefix prefix input;
+          in
+          {
+            inherit name;
+            value = config.nvim-lib.mkPlugin name inputs.${input};
+          }
+        ))
+        builtins.listToAttrs
+      ];
+  };
+
+  config.settings.config_directory = ./.;
+
+  config.specs.general = {
+    runtimePkgs = with pkgs; [
+      # LSP servers
+      lua-language-server
+      tinymist
+      clang-tools
+      pyright
+      nixd
+      typescript-language-server
+      vscode-langservers-extracted
+      vue-language-server
+      eslint_d
+      # Formatters
+      stylua
+      python3Packages.black
+      cmake-format
+      prettierd
+      typstyle
+      # Tools
+      lazygit
+      tree-sitter
+    ];
+    data = let vp = pkgs.vimPlugins; in [
+      vp."catppuccin-nvim"
+      vp."alpha-nvim"
+      vp."nvim-notify"
+      vp."indent-blankline-nvim"
+      vp."neo-tree-nvim"
+      vp."nui-nvim"
+      vp."plenary-nvim"
+      vp."nvim-web-devicons"
+      vp."which-key-nvim"
+      vp."toggleterm-nvim"
+      vp."persisted-nvim"
+      vp."guess-indent-nvim"
+      vp."nvim-lspconfig"
+      vp."conform-nvim"
+      vp."fidget-nvim"
+      vp."nvim-treesitter-textobjects"
+      vp."telescope-nvim"
+      vp."telescope-file-browser-nvim"
+      vp."telescope-project-nvim"
+      vp."typst-preview-nvim"
+      # Treesitter + grammars
+      (vp."nvim-treesitter".withPlugins (
+        plugins: with plugins; [
+          nix lua c cpp python javascript typescript tsx
+          html css json bash markdown regex yaml toml
+          go rust sql typst vue scss php cmake
+        ]
+      ))
+      # Completion
+      vp."coq_nvim"
+      vp."coq-artifacts"
+      vp."coq-thirdparty"
+    ];
+  };
+
+  # External plugins not in nixpkgs
+  config.specs.external = {
+    data = [
+      (config.nvim-lib.mkPlugin "sqlite.lua" inputs.sqlite-lua)
+      (config.nvim-lib.mkPlugin "telescope-zotero.nvim" inputs.telescope-zotero)
+    ];
+  };
+
+  # LSP server names (passed to nvim via info plugin)
+  config.info.lspServers = [
+    "lua_ls"
+    "tinymist"
+    "clangd"
+    "pyright"
+    "nixd"
+    "tsserver"
+    "vls"
+    "html"
+    "cssls"
+    "jsonls"
+    "eslint"
+  ];
+
+  config.specMods =
+    {
+      parentSpec ? null,
+      parentOpts ? null,
+      parentName ? null,
+      config,
+      ...
+    }:
+    {
+      options.runtimePkgs = options.runtimePkgs // {
+        description = ''
+          A runtimePkgs spec field to put packages on the PATH
+          If the spec is disabled, this value will not be included in the resulting neovim derivation
+        '';
+      };
+    };
+  config.runtimePkgs = config.specCollect (acc: v: acc ++ (v.runtimePkgs or [ ])) [ ];
+}
